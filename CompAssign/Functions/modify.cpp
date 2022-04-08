5b9e3e7b22a9d01fea069787734d6e4c60bd7f8d
Cytin7 <Cytin.7@outlook.com>
2022-04-08 21:45:43 +0800

    Update CompAssign

---
+++ CompAssign/Functions/modify.cpp
@@ -0,0 +1,354 @@
+//created by Yang Peidong 2022/04/07
+//
+#include <stdio.h>
+#include <iostream>
+#include "../head.h"
+#include <cstdlib>
+#include <ctime>
+using std::cout;
+using std::cin;
+
+void modify_operation(int modified_id, int information_type, int new_information,
+	FibHeap* ddl_heap, FibHeap* main_heap, FibHeap* medium_risk_heap, FibHeap* high_risk_heap, FibHeap* withdraw_heap)
+{
+	FibNode* modified_node_ddl = ddl_heap->idsearch(ddl_heap->getMin(), modified_id);
+	FibNode* modified_node_main = main_heap->idsearch(main_heap->getMin(), modified_id);
+	FibNode* modified_node_medium = medium_risk_heap->idsearch(medium_risk_heap->getMin(), modified_id);
+	FibNode* modified_node_high = high_risk_heap->idsearch(high_risk_heap->getMin(), modified_id);
+	FibNode* modified_node_withdraw = withdraw_heap->idsearch(withdraw_heap->getMin(), modified_id);
+	//1. the node is not in any heap
+	if (modified_node_ddl == NULL && modified_node_main == NULL && modified_node_medium == NULL &&
+		modified_node_high == NULL && modified_node_withdraw == NULL)
+	{
+		cout << "There is no such patient with identificaltion " << modified_id << " in all heaps.\n";
+		return;
+	}
+	//2. the node is in ddl and main heap
+	else if (modified_node_ddl != NULL && modified_node_main != NULL)
+	{
+		cout << "The patient with identification " << modified_id << " is in main and ddl heap.\n";
+		bool x = modify_for_main_and_ddl(modified_node_ddl, modified_node_main, information_type, new_information,
+			ddl_heap, main_heap, medium_risk_heap, high_risk_heap);
+		if (x) //modify_for_main_and_ddl(...) returns true
+		{
+			cout << "Modify the information of the patient with identification " << modified_id << " succcessfully.\n";
+			if (information_type == 1)//2. modify risk
+			{
+				cout << "Specifically, risk status: " << modified_node_ddl->getRecordRisk() << "->" << new_information << ".\n";
+			}
+			else if (information_type == 2)//3. modify profession
+			{
+				cout << "Specifically, profession category: " << modified_node_ddl->getRecordProfession() << "->" << new_information << ".\n";
+			}
+		}
+		else  //modify unsuccessfully
+		{
+			cout << "The new information is out of range.\n";
+		}
+	}
+	//3. the node is in medium risk heap
+	else if (modified_node_medium != NULL)
+	{
+		cout << "The patient with identification " << modified_id << " is in medium risk heap.\n";
+		bool y = modify_for_medium_risk(modified_node_medium, information_type, new_information,
+			ddl_heap, main_heap, high_risk_heap);
+		if (y) //modify_for_medium_risk(...) returns true
+		{
+			cout << "Modify the information of the patient with identification " << modified_id << " succcessfully.\n";
+			if (information_type == 1)//2. modify risk
+			{
+				cout << "Specifically, risk status: " << modified_node_medium->getRecordRisk() << "->" << new_information << ".\n";
+			}
+			else if (information_type == 2)//3. modify profession
+			{
+				cout << "Specifically, profession category: " << modified_node_medium->getRecordProfession() << "->" << new_information << ".\n";
+			}
+		}
+		else  //modify unsuccessfully
+		{
+			cout << "You may type a wrong information type or the new information is out of range.\n";
+		}
+	}
+	//4. the node is in high risk heap
+	else if (modified_node_high != NULL)
+	{
+		cout << "The patient with identification " << modified_id << " is in high risk heap.\n";
+		bool z = modify_for_high_risk(modified_node_high, information_type, new_information,
+			ddl_heap, main_heap, medium_risk_heap);
+		if (z) //modify_for_medium_risk(...) returns true
+		{
+			cout << "Modify the information of the patient with identification " << modified_id << " succcessfully.\n";
+			if (information_type == 1)//2. modify risk
+			{
+				cout << "Specifically, risk status: " << modified_node_high->getRecordRisk() << "->" << new_information << ".\n";
+			}
+			else if (information_type == 2)//3. modify profession
+			{
+				cout << "Specifically, profession category: " << modified_node_high->getRecordProfession() << "->" << new_information << ".\n";
+			}
+		}
+		else  //modify unsuccessfully
+		{
+			cout << "You may type a wrong information type or the new information is out of range.\n";
+		}
+	}
+	//5. the node is in withdraw heap
+	else if (modified_node_withdraw != NULL)
+	{
+		cout << "The patient with identification " << modified_id << " is in withdraw heap.\n";
+		bool w = modify_for_withdraw(modified_node_withdraw, information_type, new_information);
+		if (w) //modify_for_withdraw(...) returns true
+		{
+			cout << "Modify the information of the patient with identification " << modified_id << " succcessfully.\n";
+			if (information_type == 1)//2. modify risk
+			{
+				cout << "Specifically, risk status: " << modified_node_withdraw->getRecordRisk() << "->" << new_information << ".\n";
+			}
+			else if (information_type == 2)//3. modify profession
+			{
+				cout << "Specifically, profession category: " << modified_node_withdraw->getRecordProfession() << "->" << new_information << ".\n";
+			}
+		}
+		else  //modify unsuccessfully
+		{
+			cout << "You may type a wrong information type or the new information is out of range.\n";
+		}
+	}
+	//it should have been only 5 conditions above.
+	else
+	{
+		cout << "WRONG PROCESS!!!\n";
+	}
+}
+
+
+bool modify_for_main_and_ddl(FibNode* node_ddl, FibNode* node_main, int information_type, int new_information,
+	FibHeap* ddl_heap, FibHeap* main_heap, FibHeap* medium_risk_heap, FibHeap* high_risk_heap)
+	//the node is in main and ddl heap (risk = 0-1)
+	//0 - main_heap; 1 - ddl_heap; 2 - medium_risk_heap; 3 - high_risk_heap; 4 - withdraw_heap;
+	//remove processing: 1. remove; 2. update information; 3. update status; 4. re-calculate priority; 5. insert
+{
+	//modify risk(0-3)
+	//1. new infromation = old information, do nothing
+	//2. new information = 0-1, the node will not be moved, since it will be also in main and ddl heap
+	//3. new information = 2-3, the node will be moved
+	if (information_type == 1)
+	{
+		if (new_information == node_ddl->getRecordRisk()) //new information = old information
+		{
+			return true;
+		}
+		if (new_information == 0 || new_information == 1)  //also in main and ddl heap
+		{
+			node_ddl->setRecordRisk(new_information);
+			node_main->setRecordRisk(new_information);
+			return true;
+		}
+		else if (new_information == 2)  //move in medium risk heap
+		{//we remove the 2 nodes out of main and ddl heap, and then move node_main into medium risk heap
+			ddl_heap->remove(node_ddl);
+			main_heap->remove(node_main);
+			node_main->setRecordRisk(new_information);
+			node_main->status = 2;
+			node_main->calc_priority_number();
+			medium_risk_heap->Insert(node_main);
+			return true;
+		}
+		else if (new_information == 3)  //move in high risk heap
+		{//we remove the 2 nodes out of main and ddl heap, and then move node_main into high risk heap
+			ddl_heap->remove(node_ddl);
+			main_heap->remove(node_main);
+			node_main->setRecordRisk(new_information);
+			node_main->status = 3;
+			node_main->calc_priority_number();
+			high_risk_heap->Insert(node_main);
+			return true;
+		}
+		else  //new information out of range
+		{
+			return false;
+		}
+	}
+	//modify profession category(0-7)
+	//we do not need to move any node into another heap, but we should update the priority of node main
+	else if (information_type == 2)
+	{
+		if (new_information >= 0 && new_information <= 7) //new information in range
+		{
+			node_ddl->setRecordProfession(new_information);
+			node_main->setRecordProfession(new_information);
+			int temp = node_main->get_priority_number();  //update the priority of main/*******WHAT IS THE UEASG OF THE FUNCTION
+			node_main->calc_priority_number();
+			main_heap->update(node_main, temp);
+			return true;
+		}
+		else //new information out of range
+		{
+			return false;
+		}
+	}
+	//wrong information type
+	else
+	{
+		return false;
+	}
+}
+
+bool modify_for_medium_risk(FibNode* node_medium, int information_type, int new_information,
+	FibHeap* ddl_heap, FibHeap* main_heap, FibHeap* high_risk_heap)
+	//the node is in medium risk heap
+{
+	//modify risk(0-3)
+	//1. new information = old information, do nothing
+	//2. new information = 0-1, the node will be moved into main and ddl heap, here we need to create a new node
+	//3. new information = 3, the node will be moved into high risk heap
+	if (information_type == 1)
+	{
+		if (new_information == node_medium->getRecordRisk()) //new information = old information
+		{
+			return true;
+		}
+		if (new_information == 0 || new_information == 1) //move medium node into main heap, insert the new node into ddl heap
+		{
+			node_medium->setRecordRisk(new_information);
+			FibNode* new_node_ddl = new FibNode;//we do not use new
+			*new_node_ddl = *node_medium;   //copy all information******DEEP COPY OR SHALLOW COPY???
+			node_medium->status = 0; //0 - main heap
+			node_medium->calc_priority_number();
+			main_heap->Insert(node_medium);
+			new_node_ddl->status = 1; //1 - ddl heap
+			new_node_ddl->calc_priority_number();
+			ddl_heap->Insert(new_node_ddl);
+			return true;
+		}
+		else if (new_information == 3)  //move the node into high risk heap
+		{
+			node_medium->setRecordRisk(new_information);
+			node_medium->status = 3; //3 - high risk heap
+			node_medium->calc_priority_number();
+			main_heap->Insert(node_medium);
+			return true;
+		}
+		else  //new information out of range
+		{
+			return false;
+		}
+	}
+	//modify profession category(0-7)
+	//we do not need to move any node into another heap, nor update the priority of the node
+	else if (information_type == 2)
+	{
+		if (new_information >= 0 && new_information <= 7) //new information in range
+		{
+			node_medium->setRecordProfession(new_information);
+			return true;
+		}
+		else //new information out of range
+		{
+			return false;
+		}
+	}
+	//wrong information type
+	else
+	{
+		return false;
+	}
+}
+
+bool modify_for_high_risk(FibNode* node_high, int information_type, int new_information,
+	FibHeap* ddl_heap, FibHeap* main_heap, FibHeap* medium_risk_heap)
+	//the node is in high risk heap
+{
+	//modify risk(0-3)
+	//1. new information = old information, do nothing
+	//2. new information = 0-1, the node will be moved into main and ddl heap, here we need to create a new node
+	//3. new information = 2, the node will be moved into medium risk heap
+	if (information_type == 1)
+	{
+		if (new_information == node_high->getRecordRisk()) //new information = old information
+		{
+			return true;
+		}
+		if (new_information == 0 || new_information == 1) //move node_high into main heap, insert the new node into ddl heap
+		{
+			node_high->setRecordRisk(new_information);
+			FibNode* new_node_ddl = new FibNode;
+			*new_node_ddl = *node_high;   //copy all information******DEEP COPY OR SHALLOW COPY???
+			node_high->status = 0; //0 - main heap
+			node_high->calc_priority_number();
+			main_heap->Insert(node_high);
+			new_node_ddl->status = 1; //1 - ddl heap
+			new_node_ddl->calc_priority_number();
+			ddl_heap->Insert(new_node_ddl);
+			return true;
+		}
+		else if (new_information == 2)  //move the node into high risk heap
+		{
+			node_high->setRecordRisk(new_information);
+			node_high->status = 2; //3 - medium risk heap
+			node_high->calc_priority_number();
+			main_heap->Insert(node_high);
+			return true;
+		}
+		else  //new information out of range
+		{
+			return false;
+		}
+	}
+	//modify profession category(0-7)
+	//we do not need to move any node into another heap, nor update the priority of the node
+	else if (information_type == 2)
+	{
+		if (new_information >= 0 && new_information <= 7) //new information in range
+		{
+			node_high->setRecordProfession(new_information);
+			return true;
+		}
+		else //new information out of range
+		{
+			return false;
+		}
+	}
+	//wrong information type
+	else
+	{
+		return false;
+	}
+}
+
+bool modify_for_withdraw(FibNode* node_withdraw, int information_type, int new_information)
+//the node is in withdraw heap
+{
+	//modify risk(0-3)
+	//we only need to update the information
+	if (information_type == 1)
+	{
+		if (new_information >= 0 && new_information <= 3)  //new information in range
+		{
+			node_withdraw->setRecordRisk(new_information);
+			return true;
+		}
+		else  //new information out of range
+		{
+			return false;
+		}
+	}
+	//modify profession category(0-7)
+	else if (information_type == 2)
+	{
+		if (new_information >= 0 && new_information <= 7)  //new information in range
+		{
+			node_withdraw->setRecordProfession(new_information);
+			return true;
+		}
+		else  //new information out of range
+		{
+			return false;
+		}
+	}
+	//wrong information type
+	else
+	{
+		return false;
+	}
+}
---
+++ CompAssign/head.h
@@ -0,0 +1,365 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <ctime>
+#include <conio.h>
+
+#include<windows.h> // For better report performance
+using namespace std;
+
+extern time_t current_time;
+
+// global variables
+const time_t HALF_DAY = 43200;              // half day is 43200 seconds
+
+
+/* ---------------------------------------
+ * class: Record
+ *
+ * created by CYTIN
+ *
+ * Introduction:
+ *	Store all data of a recoord.
+ * --------------------------------------- */
+class Record
+{
+	// variables
+protected:
+	int id;                    // id of the patient
+	string name;               // name of the patient
+	string address;            // family address
+	string phone;              // phone number
+	string wechat;             // WeChat number
+	string email;              // e-mail address
+	time_t birth;              // date of birth
+	int age;                   // age
+	int age_group;             // age group (0~6)
+	int profession;            // profession code (0~7)
+	int risk;                  // risk status (0~3)
+	time_t deadline;           // deadline
+	int priority;              // priority, used for the priority queue
+	int registry;             // register place code
+	time_t reg_time;           // regist time
+	time_t appoint_time;       // appointment time
+	int hospital;              // appointed hospital code
+public:
+	Record();
+	~Record();
+
+	// set Record data
+	void setRecordData(int id, string name, string address, string phone, string wechat, string email, time_t birth, int profession, int risk, int registry);
+	// set Record data by variables
+	void setRecordID(int id);
+	void setRecordName(string name);
+	void setRecordAddress(string address);
+	void setRecordPhone(string phone);
+	void setRecordWeChat(string wechat);
+	void setRecordEmail(string email);
+	void setRecordBirth(time_t birth);
+	void setRecordProfession(int profession);
+	void setRecordRisk(int risk);
+	void setRecordRegistry(int registry);
+	void setRecordRegisterTime(time_t reg_time);
+	void setRecordHospital(int hospital);
+
+	// get Record data by variables
+	int getRecordID();
+	string getRecordName();
+	string getRecordAddress();
+	string getRecordPhone();
+	string getRecordWeChat();
+	string getRecordEmail();
+	time_t getRecordBirth();
+	int getRecordAge();
+	int getRecordAgeGroup();
+	int getRecordProfession();
+	int getRecordRisk();
+	int getRecordRegistry();
+	time_t getRecordRegisterTime();
+	time_t getRecordDeadline();
+	int getRecordPriority();
+	time_t getRecordAppointTime();
+	int getRecordHospital();
+
+private:
+
+};
+
+/* ---------------------------------------
+ * class: ListNode
+ *
+ * created by CYTIN
+ *
+ * Introduction:
+ *	The basis of queues.
+ *  Both registry and
+ * ---------------------------------------
+ */
+class ListNode : public Record
+{
+//public:
+//	// For file input and output
+//	fstream* file;
+protected:
+	ListNode* forward;
+	ListNode* backward;
+	bool is_dummy;       // Bool variable, true means it is a dummy node
+	int length;          // Linked list len
+
+	// For Hospital, add its variable
+	int capacity;          // The maximal number a hospital can handle
+	ListNode* last_week;   // the node has been pushed last week
+	ListNode* last_month;  // the node has been pushed last month
+
+	// For Registry, add its variable
+	ListNode* old;         // the node has been pushed to the main heaps
+
+public:
+	// construct and destruct function
+	ListNode();
+	~ListNode();
+
+	// For all nodes
+public:
+	ListNode* getForward();
+	ListNode* getBackward();
+	bool isDummy();
+
+	// For the list(dummy node):
+public:
+	// Get List information
+	int getLength();
+
+	// Set the node as the dummy node of the list
+	void setDummy();
+
+	// append a new node to the list
+	void append(ListNode* new_node);
+
+	// delete the node from the list
+	void deleteNode();
+	void deleteNode(ListNode* node);
+
+	// concat two lists
+	void concat(ListNode* list2);
+
+	// Set registry and hospital variables
+	void setCapacity(int capacity);
+	void setLastWeek(ListNode* last_week);
+	void setLastMonth(ListNode* last_month);
+	void setOld(ListNode* old);
+	void setFile(fstream* file);
+
+	// Get registry and hospital variables
+	int getCapacity();
+	ListNode* getLastWeek();
+	ListNode* getLastMonth();
+	ListNode* getOld();
+	fstream* getFile();
+};
+
+
+//
+// FibHeap Created By FDChick 17:14 4.4.22.
+//
+//
+// Class FibNode of the FibHeap.
+
+class FibNode :public Record {
+public:
+	FibNode();
+	~FibNode();
+	// FibHeap node struct
+	// Created by FDChick.
+	// defining a circle sibling linked list.
+	void setLeftSib(FibNode* node);
+	void setRightSib(FibNode* node);
+	// Pointing to Parent and one of the Child node
+	void setParent(FibNode* node);
+	void setChild(FibNode* node);
+	FibNode* getLeftSib();
+	FibNode* getRightSib();
+	// Pointing to Parent and one of the Child node
+	FibNode* getParent();
+	FibNode* getChild();
+	// 计算优先级的值
+	int get_priority_number();
+	void calc_priority_number();
+	void set_priority_number(int a);
+	int getdegree();
+	// The degree of the node
+	int degree;
+	// using when deleting node in the FibHeap.
+	bool mark_status;
+private:
+	// FibHeap node struct
+	// Created by FDChick.
+	// defining a circle sibling linked list.
+	int priority_number;
+	FibNode* LeftSib;
+	FibNode* RightSib;
+	// Pointing to Parent and one of the Child node
+	FibNode* Parent;
+	FibNode* Child;
+public:
+	int status;   // in which heap the node is in
+	FibNode* deepcopy();   // Added by CYTIN, deepcopy the node and return a pointer.
+
+// Added because of debugging
+public:
+	void priority_change();
+	void MinusOne();
+};
+
+//
+// Class FibHeap defining operations.
+//
+class FibHeap {
+public:
+
+	// construct & deconstruct
+	FibHeap();
+	~FibHeap();
+	// 移除某个节点出双链表
+	void removeNode(FibNode* node);
+	// Operations for insertion.
+	// To insert the new node just before the root linked list.
+	// Then compare min and this node,
+	// if the node is smaller,
+	// then refresh the min to be the insert node.
+	/* The Fib_Node_Add is used to complete the linked list operation.
+	 * And the Insert is truly insertion. */
+	void Add_Node(FibNode* node, FibNode* root);
+	// 通过节点指针插入节点
+	void Insert(FibNode* node);
+	// 移除斐波那契堆中的最小节点
+	void removeMin();
+	// 将other合并到当前堆中
+	/*
+	 * // 将other合并到当前堆中
+	 * void combine(FibHeap* other);
+	 */
+	 // 将斐波那契堆中的node更新为newkey
+	void update(FibNode* node, int temp);
+	// 删除结点node
+	void remove(FibNode* node);
+	// 打印斐波那契堆!
+	void print();
+	// 销毁
+	//void destroy();
+
+	// Operation to pop out the min node.
+	// Directly to connect the min-node's sub-tree in to root list.
+	// concat all the trees that has the same degree until
+	// there is no trees have the same degree.
+	FibNode* Pop_Min();
+	FibNode* extractMin();
+	/*
+	 * These Three are the operation to
+	 * consolidate the trees that have
+	 * the same degrees.
+	 */
+	void link_Node(FibNode* node, FibNode* root);
+	// renew the degree.
+	void renewDegree(FibNode* parent, int degree);
+	void Cons_Make();
+	void Consolidate();
+	// 将node从父节点parent的子链接中剥离出来，并使node成为"堆的根链表"中的一员。
+	void cut(FibNode* node, FibNode* parent);
+	// 对节点node进行"级联剪切"
+	void cascadingCut(FibNode* node);
+	// 将斐波那契堆中节点node的值减少为key
+	void decrease(FibNode* node, int key);
+	// 将斐波那契堆中节点node的值增加为key
+	void increase(FibNode* node, int key);
+	// 遍历查找键值为key的节点
+	FibNode* idsearch(FibNode* root, int id);
+	FibNode* getMin();
+private:
+	// 销毁斐波那契堆
+	//void destroyNode(FibNode* node);
+	// 打印斐波那契堆
+	void print(FibNode* node, FibNode* prev, int direction);
+	// some characters.
+	int numitems;
+	int maxDegree;
+	FibNode* min;
+	FibNode** cons;
+
+	// Peidong
+public:
+	bool withdraw(FibNode* withdraw_node, FibHeap* main_heap1, FibHeap* main_heap2, FibHeap* withdraw_heap);
+	bool modify(FibNode* modified_node, int information_type, int new_information, FibHeap* main_heap1, FibHeap* main_heap2, FibHeap* medium_risk_heap, FibHeap* high_risk_heap, FibHeap* withdraw_heap);
+	FibNode* find_node(int id, FibHeap* main_heap, FibHeap* medium_risk_heap, FibHeap* high_risk_heap, FibHeap* withdraw_heap);
+	void LoopMinus(FibNode* ptr);
+	
+};
+
+///* ---------------------------------------
+// * class: PointerNode
+// *
+// * created by CYTIN
+// *
+// * Introduction:
+// *	For list of pointers;
+// * --------------------------------------- */
+//template <class T>
+//class PNode
+//{
+//protected:
+//	PNode* dummy;
+//	PNode* forward;
+//	PNode* backward;
+//	bool is_dummy;
+//	int length;
+//	T* pointer;
+//
+//public:
+//	PNode();
+//	~PNode();
+//	void set_dummy();
+//	void set_member(T* handle);
+//	void append(T* handle);
+//	void deleteNode();
+//
+//private:
+//
+//};
+
+//class Registry :public PNode<ListNode>
+//{
+//	fstream* input_file;
+//
+//};
+ 
+
+// functions that will be used
+// Time Functions
+extern tm time_diff(time_t* time1, time_t* time2); // calculate time difference
+
+extern time_t tm2time(int month, int day, int year, int hour = 0, int minute = 0, int second = 0);    // transform date by month day year to time stamp.
+// Print Functions
+extern string genProfRome(const int prof);        // generate Rome format of profession data
+extern int PrintMainPage();					    // print main menu
+extern int PrintRegistrySetupPage();				// print registry sub menu
+extern int PrintHospitalSetupPage();				// print hospital sub menu
+
+
+// Input Functions
+// Detail information in LOADFILE.cpp
+extern int CountLines(char* filename);
+extern string ReadLine(char* filename, int line);
+extern string* split(string i);
+extern string* get_birth(string i);
+extern int LoadList(string filename, ListNode* List);
+
+// Peidong
+bool modify_for_medium_risk(FibNode* node_medium, int information_type, int new_information, FibHeap* ddl_heap, FibHeap* main_heap, FibHeap* high_risk_heap);
+bool modify_for_main_and_ddl(FibNode* node_ddl, FibNode* node_main, int information_type, int new_information, FibHeap* ddl_heap, FibHeap* main_heap, FibHeap* medium_risk_heap, FibHeap* high_risk_heap);
+bool modify_for_high_risk(FibNode* node_high, int information_type, int new_information, FibHeap* ddl_heap, FibHeap* main_heap, FibHeap* medium_risk_heap);
+bool modify_for_withdraw(FibNode* node_withdraw, int information_type, int new_information);
+
+
